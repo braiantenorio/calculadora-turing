076dee76f35bb209002f6ea446531b32bcdbe523
Braian Tenorio <tenoriovidalbraian@gmail.com>
2025-11-07 18:23:27 -0300

    arreglo en multiplicacion

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ class MaquinaTuring:
 
         print("↩️  Submáquina finalizada. Cambios preservados.\n")
 
-    def ejecutar(self, delay=0.1):
+    def ejecutar(self, delay=0.01):
         """Ejecuta toda la máquina paso a paso."""
         while self.estado != self.estado_final:
             self.mostrar()
@@ -128,7 +128,7 @@ if __name__ == "__main__":
                                           "Div": divisor}
 
     iniciador = MaquinaTuring(
-            "100 110 * ",
+            " 110010 10100 * ",
             transiciones_iniciador,
             "s0","s21")
 
